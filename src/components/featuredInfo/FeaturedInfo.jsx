08fc0a9d25da2415bc2d1e0d27c7435e3d1df05b
ravi <kazuki.1209.haru@gmail.com>
2022-10-15 16:31:30 +0900

    fix: featuredInfo

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -8,7 +8,10 @@ export default function FeaturedInfo() {
             <sapn className="featuredTitle">Revanue</sapn>
             <div className="featuredMoneyContainer">
                 <span className="featuredMoney">$2,415</span>
-                <span className="featuredMoneyRate">-11.4 <ArrowDownward /></span>
+                <span className="featuredMoneyRate">
+                    -11.4 
+                    <ArrowDownward className="featuredIcon negative" />
+                </span>
             </div>
             <span className="featuredSub">Compared to last month</span>
         </div>
@@ -16,7 +19,10 @@ export default function FeaturedInfo() {
             <sapn className="featuredTitle">Sales</sapn>
             <div className="featuredMoneyContainer">
                 <span className="featuredMoney">$4,415</span>
-                <span className="featuredMoneyRate">-1.4 <ArrowDownward /></span>
+                <span className="featuredMoneyRate">
+                    -1.4 
+                    <ArrowDownward className="featuredIcon negative" />
+                </span>
             </div>
             <span className="featuredSub">Compared to last month</span>
         </div>
@@ -26,7 +32,7 @@ export default function FeaturedInfo() {
                 <span className="featuredMoney">$2,225</span>
                 <span classNaAme="featuredMoneyRate">
                     +11.4 
-                    <ArrowUpward />
+                    <ArrowUpward className="featuredIcon" />
                 </span>
             </div>
             <span className="featuredSub">Compared to last month</span>

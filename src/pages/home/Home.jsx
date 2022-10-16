0ee5f61be02768db28399f5248cf0b3606201982
ravi <kazuki.1209.haru@gmail.com>
2022-10-16 22:29:34 +0900

    fix

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,19 @@
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import Chart from '../../components/chart/Chart'
+import WidgetSm from '../../components/widgetSm/widgetSm'
+import WidgetLg from '../../components/widgetLg/widgetLg'
 import './home.css'
-import { userData } from "../../dummyData"
+import { userData } from '../../dummyData'
 
 export default function Home() {
-  return (
-    <div className='home'>
-        <FeaturedInfo/>
-        <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
-        <div className='homeWidgets'></div>
-    </div>
-  )
+    return (
+        <div className="home">
+            <FeaturedInfo />
+            <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
+            <div className="homeWidgets">
+                <WidgetSm />
+                <WidgetLg />
+            </div>
+        </div>
+    )
 }
